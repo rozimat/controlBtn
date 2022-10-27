e264ff64dfb7fad0fe806379d631591c1ed26b7a
rozimat <rozimattoshtanboyev@gmail.com>
2022-10-27 17:07:43 +0300

    second commit

--- assertup/js/app.js
+++ assertup/js/app.js
@@ -1,17 +1,23 @@
 const radios = document.getElementsByClassName('radio');
-var checked_id = 1
+const left1 =document.querySelector("#left");
+const right1 =document.querySelector("#right");
 
-function left() {
+
+left1.addEventListener('click', ()=>{
+  var checked_id = 1
   if (checked_id > 1){
     checked_id = checked_id - 1;
     document.getElementById('radion-'+checked_id).checked =true
 
   }
-}
-function right(){
+})
+
+
+right1.addEventListener('click', ()=>{
+  var checked_id = 1
   if (checked_id < radios.length){
     checked_id = checked_id + 1;
     document.getElementById('radion- '+checked_id).checked =true
     
   }
-}
+})
